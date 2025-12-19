feb4c9b8b660d95fd03d69d23eec8655f60ef568
FatmaTekinmez <fatmatekinmez@gmail.com>
2025-12-20 01:51:31 +0300

    anasayfa ve layout eklendi

--- FitnessCenter/Controllers/HomeController.cs
+++ FitnessCenter/Controllers/HomeController.cs
@@ -1,20 +1,25 @@
 using System.Diagnostics;
+using FitnessCenter.Data;
 using FitnessCenter.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace FitnessCenter.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            // Þimdilik sadece tanýtým sayfasý, veri göndermiyoruz
             return View();
         }
 
@@ -26,7 +31,10 @@ namespace FitnessCenter.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
         }
     }
 }
